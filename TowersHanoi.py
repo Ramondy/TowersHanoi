8cafb48f1f41d30b5bf6242a2e1b3d17c58f33bb
Ramondy <32699785+Ramondy@users.noreply.github.com>
2020-03-11 18:43:32 +0100

    functional implementation Computer player

--- TowersHanoi.py
+++ TowersHanoi.py
@@ -29,17 +29,78 @@ def initialize_disks(min_disks=3):
     return
 
 
+def select_stack(user_input):
+    for e in stack_board:
+        if e.get_initial() == user_input:
+            return e
+
+
 def get_user_input():
-    global stack_board
-    global choices
     while True:
         for i in range(len(choices)):
             print("Enter {0} for {1}".format(choices[i], stack_board[i].get_name()))
         user_input = input("")
         if user_input in choices:
-            for e in stack_board:
-                if e.get_initial() == user_input:
-                    return e
+            return select_stack(user_input)
+
+
+def check_move_validity(from_, to_):
+    if from_.is_empty() or not to_.has_space() or from_.peek() > to_.peek():
+        return False
+    else:
+        return True
+
+
+def record_move(from_, to_):
+    global record
+    record.append("{0} -> {1}".format(from_, to_))
+
+
+def implement_move(from_, to_):
+    global num_user_moves
+
+    disk = from_.pop()
+    to_.push(disk)
+    num_user_moves += 1
+    if player == "H":
+        print("Played from {0} to {1}".format(from_, to_))
+    return
+
+
+def determine_transit_loc(from_, to_):
+    temp = choices[:]
+    temp.remove(from_)
+    temp.remove(to_)
+    return temp.pop()
+
+
+def play_as_computer(num_, from_, to_):
+
+    if num_ == 1:
+        if check_move_validity(select_stack(from_), select_stack(to_)):
+            implement_move(select_stack(from_), select_stack(to_))
+            record_move(select_stack(from_), select_stack(to_))
+            return
+        else:
+            print("Strategy error!")
+            return
+
+    else:
+        transit_loc = determine_transit_loc(from_, to_)
+        #print(num_, transit_loc)
+
+        play_as_computer(num_ - 1, from_, transit_loc)
+        play_as_computer(1, from_, to_)
+        play_as_computer(num_ - 1, transit_loc, to_)
+    return
+
+
+def finish_game():
+    if num_user_moves == num_optimal_moves:
+        print("\nCONGRATULATIONS!")
+    print("\nYou completed the game in {0} moves, and the optimal number of moves is {1}.".
+          format(num_user_moves, num_optimal_moves))
+    print("Here is the sequence of moves: ", record)
 
 
 if __name__ == '__main__':
@@ -47,6 +108,7 @@ if __name__ == '__main__':
     num_disks= 0
     num_optimal_moves = 0
     num_user_moves = 0
+    record = []
 
     print("\nLet's play Towers of Hanoi!!")
 
@@ -54,17 +116,16 @@ if __name__ == '__main__':
     stack_board = create_board()
     choices = create_choices()
     # chose number of disks, place disks in left stack:
-    initialize_disks()
-    num_on_goal = stack_board[-1].get_size()
+    initialize_disks(2)
 
     player = ""
-    while player not in ["h", "c"]:
-        player = input("\nWho is playing? Human (h) or Computer (c) ?")
+    while player not in ["H", "C"]:
+        player = input("\nWho is playing? Human (H) or Computer (C) ?")
 
-    if player == "h":
+    if player == "H":
 
         #PLAY THE GAME
-        while num_on_goal != num_disks:
+        while stack_board[-1].get_size() != num_disks:
             print("\n...Current Stacks...")
             for stack in stack_board:
                 stack.print_items()
@@ -74,36 +135,22 @@ if __name__ == '__main__':
                 from_stack = get_user_input()
                 print("\nWhich stack do you want to move to?\n")
                 to_stack = get_user_input()
-                if from_stack.is_empty() or not to_stack.has_space() or from_stack.peek() > to_stack.peek():
-                    print("\nInvalid Move. Try Again")
-                    continue
-                else:
-                    disk = from_stack.pop()
-                    to_stack.push(disk)
-                    num_user_moves += 1
+
+                if check_move_validity(from_stack, to_stack):
+                    implement_move(from_stack, to_stack)
+                    record_move(from_stack, to_stack)
                     break
 
-        if num_user_moves == num_optimal_moves:
-            print("\nCONGRATULATIONS!")
-        print("\nYou completed the game in {0} moves, and the optimal number of moves is {1}.".
-              format(num_user_moves, num_optimal_moves))
+                if not check_move_validity(from_stack, to_stack):
+                    print("\nInvalid Move. Try Again")
+                    continue
 
-    if player == "c":
+    if player == "C":
 
-        play_as_computer()
+        while stack_board[-1].get_size() != num_disks:
+            play_as_computer(num_disks, "L", "R")
+
+    finish_game()
 
 
-        pass
 
-def play_as_computer():
-
-    global stack_board
-    global num_disks
-    global num_on_goal
-
-    even_play = (num_disks % 2 == 0)
-    num_at_play = 1 - num_on_goal
-
-    while num_on_goal != num_disks:
-        if even_play:
-        else:
